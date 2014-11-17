b66f8812364398f73cb2972317f4ac9e1a43e549
Aaron Herres, Peter DeMartini, and Roy van de Water <dev+aaron+peter+roy@integrumtech.com>
2014-11-17 14:36:29 -0700

    Route MQTT messages to Meshblu

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var util = require('util');
 var EventEmitter = require('events').EventEmitter;
+var mosca = require('mosca');
+var debug = require('debug')('meshblu-meshlium');
 
 var MESSAGE_SCHEMA = {
   type: 'object',
@@ -34,12 +36,39 @@ function Plugin(){
 util.inherits(Plugin, EventEmitter);
 
 Plugin.prototype.onMessage = function(message){
-  var payload = message.payload;
-  this.emit('message', {devices: ['*'], topic: 'echo', payload: payload});
+  if(!this.server){
+    return;
+  }
+
+  this.server.publish({topic: message.topic, qos: 0, payload: message.payload, retain: false});
 };
 
 Plugin.prototype.setOptions = function(options){
+  var self = this;
+
   this.options = options;
+  this.server = new mosca.Server({});
+
+  this.server.on('ready', function(){
+    debug('Mosca server is up and running.');
+  });
+
+  this.server.on('clientConnected', function(client){
+    debug('clientConnected', client.id);
+  })
+
+  this.server.on('clientDisconnected', function(client){
+    debug('clientDisonnected', client.id);
+  })
+
+  this.server.on('published', function(packet, client){
+    if(!client){
+      return;
+    }
+
+    debug('published', client.id, packet.topic, packet.payload.toString());
+    self.emit({devices: '*', topic: packet.topic, payload: packet.payload.toString()});
+  });
 };
 
 module.exports = {
